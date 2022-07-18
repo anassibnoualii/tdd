bcb8f211856e48e24598ac515edc840f825722eb
Anass IbnouAli <anass.ibnou-ali@alithya.com>
2022-07-18 15:08:35 +0100

    step2:Solution

--- src/main/java/com/crafts/tdd/AddNumbers.java
+++ src/main/java/com/crafts/tdd/AddNumbers.java
@@ -4,10 +4,23 @@ public class AddNumbers {
 
   private AddNumbers() {}
 
-  public static Integer add(String s) {
-    if (s.isEmpty()) return 0;
-    if (s.equals("1")) return 1;
-    if (s.equals("1,2")) return 3;
-    return null;
+  public static Integer add(String input) {
+    if (input.isEmpty()) {
+      return 0;
+    }
+    String[] numbers = input.split(",");
+    return getSum(numbers);
+  }
+
+  private static int getSum(String[] numbers) {
+    int sum = 0;
+    for (String currentNumber : numbers) {
+      sum += stringToInt(currentNumber);
+    }
+    return sum;
+  }
+
+  private static int stringToInt(String number) {
+    return Integer.parseInt(number);
   }
 }
